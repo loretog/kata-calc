c38754f379ecc8cb02189834266e209390b0fc33
Walter Dal Mut <walter.dalmut@gmail.com>
2015-08-18 23:31:28 +0200

    Refactored lexeme factory using functions

--- src/Lexeme.php
+++ src/Lexeme.php
@@ -3,48 +3,46 @@ namespace Calc;
 
 class Lexeme
 {
-    private static $numbers = ["0","1","2","3","4","5","6","7","8","9"];
-    private static $symbols = ["+", "-"];
-
     public static function fromToken($token)
     {
         $lexeme = null;
 
         if ($token === null) {
             $lexeme = new Lexeme\EqualSymbol("=");
-        } else if (self::isNumber($token)) {
+        } else if (is_number($token)) {
             $lexeme = new Lexeme\Number($token);
-        } else if (self::isSymbol($token)) {
-            $lexeme = self::getSymbol($token);
+        } else if (is_symbol($token)) {
+            $lexeme = get_symbol($token);
         }
 
         return $lexeme;
     }
+}
 
-    private static function getSymbol($token)
-    {
-        switch ($token) {
+function is_number($token)
+{
+    static $numbers = ["0","1","2","3","4","5","6","7","8","9"];
+    return in_array($token, $numbers, true);
+}
+
+function is_symbol($token)
+{
+    static $symbols = ["+", "-"];
+    return in_array($token, $symbols, true);
+}
+
+function get_symbol($token)
+{
+    $symbol = null;
+    switch ($token) {
         case '+':
             $symbol = new Lexeme\AdditionSymbol($token);
             break;
         case '-':
             $symbol = new Lexeme\SubtractSymbol($token);
             break;
-        default:
-            $symbol = new Lexeme\Symbol($token);
-            break;
-        }
-
-        return $symbol;
     }
 
-    private static function isNumber($token)
-    {
-        return in_array($token, self::$numbers, true);
-    }
-
-    private static function isSymbol($token)
-    {
-        return in_array($token, self::$symbols, true);
-    }
+    return $symbol;
 }
+
